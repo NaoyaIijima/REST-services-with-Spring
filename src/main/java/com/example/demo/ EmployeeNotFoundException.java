61d273f0ed4f30676d57e03ea04b27c1846b4489
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2019-08-26 06:20:32 +0000

    途中経過

---
+++ src/main/java/com/example/demo/ EmployeeNotFoundException.java
@@ -0,0 +1,8 @@
+package main.java.com.example.demo;
+
+class EmployeeNotFoundException extends RuntimeException {
+
+  EmployeeNotFoundException(Long id) {
+    super("Could not find employee " + id);
+  }
+}
